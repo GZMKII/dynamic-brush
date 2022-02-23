e756a2d9abab7ea2092ac0319f7c209c1a1517b5
GZMKII <jiujigong@hotmail.com>
2022-02-23 23:05:05 +0800

    touch version

--- sketch.js
+++ sketch.js
@@ -15,25 +15,30 @@ let opacity = 255
 let toggle;
 
 
-// var luzhi =  document.getElementById('start-capturing-button')
+
+
+// 初始化录制
+let autoDownloadFile = true
+
+//set to no download at the end
+let rec = new p5.Recorder(autoDownloadFile);
+
+let options = {
+    filename: "dynamic_new_year_letter.webm",
+    recordAudio: false,
+    audioBitRate: 128000,
+    videoBitRate: 100000000, //10 megabits
+    fps: 60,
+}
 
 
 
-var baocun = document.getElementById('download-video-button')
-baocun.addEventListener('pointerdown', function() {
-    console.log(123)
-        //     capturer.stop()
-        //     capturer.save()
-        // })
-        // let capturer = new CCapture({
-        //     format: 'webm',
-        //     framerate: 30,
+let baocun = document.getElementById('download-video-button')
+baocun.addEventListener('click', function() {
+    rec.stop();
+
 })
 
-// luzhi.addEventListener('click', function(){
-
-// 	capturer.start()
-// })
 
 
 //创建node
@@ -60,7 +65,7 @@ Noodle.prototype.drawing = function() {
 Noodle.prototype.update = function() {
     // this.r = this.r + sin(this.timepast)
     this.opacity = sin(this.timepast) * 200 + 100
-    this.timepast += 0.03
+    this.timepast += 0.01
         // console.log(this.opacity)
 }
 
@@ -74,58 +79,23 @@ function setup() {
     kanwasi = document.getElementById('huabu')
         // console.log(`sss: ${kanwasi}`)
     let x = y = oldX = oldY = 0
-        // enableCapture({
-        //     frameCount: 360,
-        //     onComplete: funct    ion() { noLoop() }
-        // });
-        // cnv.onClick = function() {
-        //     console.log(123)
-        // }
-    kanwasi.addEventListener("pointerdown", function() {
-        toggle = true
-    })
 
-    kanwasi.addEventListener("pointerup", function() {
-        toggle = false
-    })
 
 }
 
-// console.log(`bbs: ${kanwasi}`)
-// kanwasi.addEventListener("pointerdown", function() {
-//     console.log(123)
-// })
-
-
-
 //画
 function draw() {
-    frameRate(30)
-    background(52, 52, 52, 255)
-        // debugger
-        // timepast += 1
-        // requestAnimationFrame(draw)
-        // debugger
+    frameRate(60)
+    background(12, 12, 12, 255)
 
-    //获取cursor坐标
-    let getCursor = document.querySelectorAll('.touchfreecursor')[0]
-    let cursorX = getComputedStyle(getCursor, null).getPropertyValue("left")
-    let cursorY = getComputedStyle(getCursor, null).getPropertyValue("top")
-
-    //转换成数字
-    let cx = parseInt(cursorX, 10)
-    let cy = parseInt(cursorY, 10)
-
-
-    console.log(toggle)
-    if (toggle) {
+    if (mouseIsPressed) {
         if (!f) {
             f = true
-            x = cx
-            y = cy
+            x = mouseX
+            y = mouseY
         }
-        vx += (cx - x) * spring
-        vy += (cy - y) * spring
+        vx += (mouseX - x) * spring
+        vy += (mouseY - y) * spring
         vx *= friction
         vy *= friction
 
@@ -148,9 +118,6 @@ function draw() {
             objs.push(new Noodle(x, y, oldX, oldY, oldR))
         }
 
-        // capturer.start();
-
-
     } else if (f) {
         ax = ay = 0
         f = false
@@ -162,9 +129,10 @@ function draw() {
         objs[i].update()
     }
 
-    // capturer.capture(canvas)
-    // document.getElementById('huaban')
-    // console.log(capturer.capture(canvas))
-    // captureFrame()
+    //开始录像
+    if (objs.length === 10) {
+        rec.start(options)
+        console.log(1)
+    }
 
 }
